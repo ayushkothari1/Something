1bc343dc4683e6a26a39779523fbe180cdfca644
ayushkothari1 <pahadiayush61@gmail.com>
2026-03-10 01:02:28 +0530

    happy birthday

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-let timer = 10;
 const countEL = document.querySelector("#count");
 const countdown = document.querySelector(".countdown");
 const audioEl = document.querySelector(".audio");
-const containerEl = document.querySelector(".container");
+const messageEl = document.querySelector(".message");
+const cakeEl = document.querySelector(".cake");
+const bodyEl = document.querySelector("body");
+
+let timer = 10;
+
 countdown.addEventListener("click", function () {
-  const timerEL = setInterval(function settimer() {
+  countdown.style.pointerEvents = "none";
+  const timerEl = setInterval(function () {
     countEL.innerHTML = timer;
     timer--;
-    setInterval(function () {
-      let colorIndex = Math.floor(Math.random() * 9);
-      const color = [
-        "Red",
-        "green",
-        "yello",
-        "pink",
-        "white",
-        "brown",
-        "blue",
-        "purple",
-        "orange",
-      ];
-      // console.log(colorIndex);
-      containerEl.style.background = color[colorIndex];
-    }, 100);
-
-    if (timer === -2) {
-      countEL.style.display = "none";
-      countdown.classList.add("pros");
-      audioEl.play();
+    if (timer < 0) {
+      clearInterval(timerEl);
+      countdown.style.display = "none";
+      startParty();
     }
   }, 1000);
 });
 
-// setInterval(function () {
-//   const color = [
-//     "Red",
-//     "green",
-//     "yello",
-//     "pink",
-//     "white",
-//     "brown",
-//     "blue",
-//     "purple",
-//     "orange",
-//   ];
-//   const colorIndex = Math.floor(Math.random() * 9);
-//   // console.log(colorIndex);
-//   containerEl.style.background = color[colorIndex];
-// }, 100);
+function startParty() {
+  audioEl.play();
+  cakeEl.style.display = "block";
+  confetti({
+    particleCount: 200,
+    spread: 120,
+  });
+  typeMessage();
+  setInterval(createBallon, 500);
+}
+
+function typeMessage() {
+  const text = "🎉 Happy Birthday 🎂";
+  let index = 0;
+  const typing = setInterval(() => {
+    messageEl.innerHTML += text[index];
+    index++;
+    if (index === text.length) {
+      clearInterval(typing);
+    }
+  }, 100);
+}
+
+function createBallon() {
+  const ballon = document.createElement("div");
+  ballon.classList.add("balloon");
+  ballon.style.left = Math.random() * window.innerWidth + "px";
+  const colors = ["red", "blue", "yellow", "pink", "purple", "orange"];
+  ballon.style.background = colors[Math.floor(Math.random() * colors.length)];
+  bodyEl.appendChild(ballon);
+  (setTimeout(() => {
+    ballon.remove();
+  }),
+    6000);
+}
